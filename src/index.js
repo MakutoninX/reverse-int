221be5617f1b283ca3701969f5cfbfea7dacfe4e
Артур Макутонин <arthurmakutonin@MBP-Artur.localdomain>
2022-06-22 13:45:37 +0300

    docs: add final solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+    if(n<0){
+        let numToStr = n.toString();
+        return Array.from(numToStr.slice(1,numToStr.length)).reverse().join('');
+    }
+  let numToStr = n.toString();
+  return Array.from(numToStr).reverse().join('');
   
 }
